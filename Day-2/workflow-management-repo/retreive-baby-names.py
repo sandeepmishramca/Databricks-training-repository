12df4a4e3622718cd4a2158f72c2bc4b6ed9a70d
Sourishdatabricks175302 <Sourishdatabricks175302>
2023-02-14 16:58:32 +0000

    Created parameterized workflow repo folder

--- Day-2/workflow-management-repo/retreive-baby-names.py
+++ Day-2/workflow-management-repo/retreive-baby-names.py
@@ -3,4 +3,4 @@ import requests
 
 response = requests.get('http://health.data.ny.gov/api/views/myeu-hzra/rows.csv')
 csvfile = response.content.decode('utf-8')
-dbutils.fs.put("dbfs:/FileStore/babynames.csv", csvfile, True)
+dbutils.fs.put("dbfs:/FileStore/workflow-repo/babynames.csv", csvfile, True)
