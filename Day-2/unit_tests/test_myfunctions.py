dd222ebdd578f36ef89e6cf17a5a0d4af3ccc792
Sourishdatabricks175302 <125254222+Sourishdatabricks175302@users.noreply.github.com>
2023-02-15 10:30:22 +0530

    Update test_myfunctions.py
    modified column value

--- Day-2/unit_tests/test_myfunctions.py
+++ Day-2/unit_tests/test_myfunctions.py
@@ -6,7 +6,7 @@ from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, S
 
 tableName    = "diamonds"
 dbName       = "default"
-columnName   = "sample"
+columnName   = "sample1"
 columnValue  = "SI2"
 
 # Because this file is not a Databricks notebook, you
